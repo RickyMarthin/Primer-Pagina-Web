924ff959fcc1b12067329d7a37e2ebea6ba06e80
RickyMarthin <rarp01misterius@gmail.com>
2023-11-23 17:50:08 -0500

    problema resulto, volvi a una version anterior de handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 // Inicializacion
 import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import { fileURLToPath } from 'url';
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'));
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
+// Middlerwares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res)=>{
+    res.json({"message": "Hola"});
+})
+
+// Public files
+app.use(express.static(join(__dirname, 'public')));
+
+// Run server
 app.listen(app.get('port'), () =>
     console.log('Server listening on port: ', app.get('port')));
