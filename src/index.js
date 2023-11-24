7fdaa87ac75557e875e933d8732d6df33b0cb4f5
RickyMarthin <rarp01misterius@gmail.com>
2023-11-23 19:04:45 -0500

    index, navigation e index.hbs agregados, tambien conexion database.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'));
-app.engine('handlebars', engine({
+app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: join(app.get('views'), 'layouts'),
     partialsDir: join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
-app.set('view engine', '.hbs');
+app.set('view engine', '.hbs'); 
 
 // Middlerwares
 app.use(morgan('dev'));
@@ -25,7 +25,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res)=>{
-    res.json({"message": "Hola"});
+    res.render('index')
 })
 
 // Public files
